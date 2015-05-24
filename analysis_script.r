08386a774fc31e1093e61c2379c2b67c297bc1df
Tony G. Bolaño <tonybolanyo@gmail.com>
2015-05-24 08:45:08 +0200

    First commit

---
+++ analysis_script.r
@@ -0,0 +1,11 @@
+# First we need download data
+dataurl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+datafile <- "StormData.csv.bz2"
+
+if (!file.exists(dataurl)) {
+    download.file(dataurl,datafile)
+}
+
+# R can read csv files form bzip compressed files directly so
+
+storm <- read.csv(datafile)
